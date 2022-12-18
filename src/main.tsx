fbc307ad9eee09bfb787989a69a105799b4bf693
j-w-park <okml133@gmail.com>
2022-12-18 23:25:24 +0900

    add comment

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import { RecoilRoot } from "recoil";
 import { App } from "./App";
 
 const initialize = async () => {
+  // 개발 환경에서만 MSW가 활성화 되도록 함
   if (import.meta.env.DEV) {
-    const { worker } = await import("./mocks/browser");
+    const { worker } = await import("./mocks/browser"); // 동적 임포트
     return worker.start();
   }
   return Promise.resolve();
