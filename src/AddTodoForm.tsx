2b36e4bd74f7616478fb1e2abf69dc8d0a12528f
j-w-park <okml133@gmail.com>
2022-12-19 02:16:48 +0900

    optimistic update when adding todo

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,29 +1,73 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 import { nanoid } from "nanoid";
 import { useRef } from "react";
-import { useRecoilCallback } from "recoil";
-import { todoKeysAtom, todoAtom } from "./atom/todo";
+import { useRecoilCallback, useSetRecoilState } from "recoil";
+import { todoListAtom, todoItemAtom } from "./atom/todo";
+import { Todo } from "./mocks/types";
 
 export const AddTodoForm = () => {
-  const todoInputElementRef = useRef<HTMLInputElement>(null);
+  const titleRef = useRef<HTMLInputElement>(null);
 
-  const addTodo = useRecoilCallback(({ set }) => () => {
-    const { current: todoInputElement } = todoInputElementRef;
-    if (!todoInputElement) {
-      return;
-    }
-    const todoKey = nanoid();
-    set(todoKeysAtom, (prev) => [...prev, todoKey]);
-    set(todoAtom(todoKey), { title: todoInputElement.value, done: false });
-    todoInputElement.focus();
+  const queryClient = useQueryClient();
+
+  const addTodoOptimistically = useRecoilCallback(
+    ({ set, snapshot }) =>
+      async (args: { title: string }) => {
+        // optimistic update가 덮어 씌워지지 않게 진행중인 refetch를 모두 취소함
+        await queryClient.cancelQueries({ queryKey: ["todo"] });
+
+        // Snapshot the previous value
+        const prevTodoList = snapshot.getLoadable(todoListAtom).getValue();
+
+        // Optimistically update to the new value
+        const newTodo = { id: nanoid(), title: args.title, done: false };
+        set(todoListAtom, (old) => [...old, newTodo]);
+        set(todoItemAtom(newTodo.id), newTodo);
+
+        // Return a context object with the snapshotted value
+        return { prevTodoList };
+      }
+  );
+
+  const setTodoList = useSetRecoilState(todoListAtom);
+
+  const { mutate: addTodo } = useMutation({
+    mutationKey: ["todo", "add"],
+    mutationFn: (args: { title: string }) =>
+      axios
+        .post("/todo", { title: args.title }, { responseType: "json" })
+        .then((res) => res.data),
+
+    // mutate 함수가 호출될 때 실행됨
+    onMutate: addTodoOptimistically,
+
+    // mutation이 실패했을 때 실행됨
+    // onMutate에서 반환한 context를 받아서 이전 상태로 되돌림
+    onError: (err, args, context) => {
+      if (context?.prevTodoList) {
+        setTodoList(context?.prevTodoList);
+      }
+    },
+
+    // mutation이 끝나면 refetch
+    // error든 success든 상관 없음
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["todo"] });
+    },
+
+    onSuccess: () => {
+      titleRef.current?.focus();
+    },
   });
 
   return (
     <form>
-      <input ref={todoInputElementRef} type="text" />
+      <input ref={titleRef} type="text" />
       <button
         onClick={(e) => {
           e.preventDefault();
-          addTodo();
+          addTodo({ title: titleRef.current?.value ?? "" });
         }}
         type="submit"
       >
