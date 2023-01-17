ba92bacef02d5c9934151c01ab00c046adefb9b9
j-w-park <okml133@gmail.com>
2023-01-17 21:36:49 +0900

    unhandled request는 무시하도록 설정

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const initialize = async () => {
   // 개발 환경에서만 MSW가 활성화 되도록 함
   if (import.meta.env.DEV) {
     const { worker } = await import("./mocks/browser"); // 동적 임포트
-    return worker.start();
+    return worker.start({ onUnhandledRequest: "bypass" });
   }
   return Promise.resolve();
 };
